815bb0cc0ddf9bc632a1524c6562a8f7146a514b
zNotChill <znotchill@znci.dev>
2022-09-19 01:28:36 +0100

    maid costume

--- public/admin/recentLinks.js
+++ public/admin/recentLinks.js
@@ -1,7 +1,9 @@
 async function getAllLinks() {
+	document.getElementById("refresh-links").style.visibility = "hidden"
 	document.querySelector(".recent-links-container").innerHTML = `<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`
 	setTimeout(() => {
 		fetch("/api/getAllLinks").then((res) => res.json()).then(json => {
+			document.getElementById("refresh-links").style.visibility = "inherit"
 			document.querySelector(".recent-links-container").innerHTML = ``
 			json.forEach(v => {
 				document.querySelector(".recent-links-container").innerHTML += `
@@ -9,6 +11,7 @@ async function getAllLinks() {
 						<div class="action">Created link</div>
 						<div class="info">${v.shortId} &bull; ${v.redirectUrl}</div>
 						<p class="time">${$.timeago(v.time)}</p>
+						<button class="delete">Delete</button>
 					</div>
 				`
 			});
